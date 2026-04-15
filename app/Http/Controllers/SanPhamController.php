af49b5f6c97fffac218532a0be1f1b576e574db6
nq-hub <030239230202@st.buh.edu.vn>
2026-04-15 20:41:01 +0700

    yc5

--- app/Http/Controllers/SanPhamController.php
+++ app/Http/Controllers/SanPhamController.php
@@ -17,36 +17,5 @@ class SanPhamController extends Controller
 
     return view('caycanh.index', compact('products', 'keyword'));
 }
-    public function create()
-    {
-        return view('product.create');
-    }
-    public function store(Request $request)
-{
-    $product = new SanPham();
-
-    $product->ten_san_pham = $request->ten_san_pham;
-    $product->ten_khoa_hoc = $request->ten_khoa_hoc;
-    $product->ten_thong_thuong = $request->ten_thong_thuong;
-    $product->mo_ta = $request->mo_ta;
-    $product->do_kho = $request->do_kho;
-    $product->anh_sang = $request->anh_sang;
-    $product->nuoc = $request->nuoc;
-    $product->gia = $request->gia;
-
-    // upload ảnh
-    if ($request->hasFile('hinh_anh')) {
-        $file = $request->file('hinh_anh');
-        $filename = time() . '.' . $file->getClientOriginalExtension();
-        $file->storeAs('public/image', $filename);
-        $product->hinh_anh = $filename;
-    }
-
-    $product->status = 1;
-
-    $product->save();
-
-    return redirect('/')->with('success', 'Thêm thành công');
-}
-
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@ Route::get('/dashboard', function () {
     //return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/timkiem', ['App\Http\Controllers\SanPhamController', 'search'])->name('search');
-Route::get('/product/create', [App\Http\Controllers\SanPhamController::class, 'create'])->name('product.create');
-Route::post('/product/store', [App\Http\Controllers\SanPhamController::class, 'store'])->name('product.store');
+
+Route::get('/sanpham/{id}', [SanPhamController::class, 'detail'])->name('sanpham.detail');
 
 require __DIR__.'/auth.php';
+
+
